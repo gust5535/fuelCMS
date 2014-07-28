1c8dedc62f91de297614a42a346ddf8552f9e3ed
Andriy Leshchuk <aleshchuk@crsinc.com>
2014-07-28 19:06:34 +0300

    FuelCMS + CodeIgniter framework initial commit

---
+++ fuel/application/cache/dwoo/compiled/0c3330437f0410b24b02c7a1d6dd8557.d17.php
@@ -0,0 +1,12 @@
+<?php
+/* template head */
+/* end template head */ ob_start(); /* template body */ ?>testtesttesttesttesttesttesttestt
+esttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttesttestt
+esttesttesttesttesttesttesttestte
+sttesttesttesttesttes
+
+<img src="<?php echo img_path('grib.jpg');?>" width="100" height="100" alt="grib" />
+
+ttesttesttesttesttesttesttesttesttesttest<?php  /* end template body */
+return $this->buffer . ob_get_clean();
+?>
